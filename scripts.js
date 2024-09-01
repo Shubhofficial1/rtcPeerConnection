5948dd10b81e094eff85882760b333850455aacd
shubham kumar <shubhofficial1@gmail.com>
2024-09-01 14:38:49 +0530

    Added Local RTC

--- scripts.js
+++ scripts.js
@@ -5,12 +5,50 @@ let localStream;
 let remoteStream;
 let peerConnection;
 
+let peerConfiguration = {
+  iceServers: [
+    {
+      urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"],
+    },
+  ],
+};
+
 const call = async (e) => {
   const stream = await navigator.mediaDevices.getUserMedia({
     video: true,
     // audio: true,
   });
   localVideoEl.srcObject = stream;
+  localStream = stream;
+
+  // PeerConnection is all set with out STUN servers sent over
+  await createPeerConnection();
+  //   Create Offer
+
+  try {
+    console.log("............Creating Offer.............");
+    const offer = await peerConnection.createOffer();
+    console.log(offer);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const createPeerConnection = () => {
+  return new Promise(async (resolve, reject) => {
+    peerConnection = await new RTCPeerConnection(peerConfiguration);
+
+    // Associate our stream with the peer connection
+    localStream.getTracks().forEach((track) => {
+      peerConnection.addTrack(track, localStream);
+    });
+
+    peerConnection.addEventListener("icecandidate", (e) => {
+      console.log("................ICE CANDIDATE FOUND................");
+      console.log(e);
+    });
+    resolve();
+  });
 };
 
 document.querySelector("#call").addEventListener("click", call);
