37358a9aacef86c3912c237ac86b3e059cfc8db5
shubham kumar <shubhofficial1@gmail.com>
2024-09-01 15:15:57 +0530

    Socket.io & HTTPS setup

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+const https = require("https");
+const fs = require("fs");
 const express = require("express");
 const app = express();
+const socketio = require("socket.io");
 app.use(express.static(__dirname));
-app.listen(9000, () => {
+
+const key = fs.readFileSync("cert.key");
+const cert = fs.readFileSync("cert.crt");
+
+const expressServer = https.createServer({ key, cert }, app);
+const io = socketio(expressServer);
+expressServer.listen(9000, () => {
   console.log("Server is running on port 9000");
+});
+
+io.on("connection", () => {
+  console.log("Someone has connected");
 });
